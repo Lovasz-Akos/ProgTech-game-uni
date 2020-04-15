fa6837a23b41f14d1f3370f43c3351401808fff7
Lovász Ákos <akos4x44@gmail.com>
2020-04-15 16:50:34 +0200

    Moved description to README.md

--- src/renameThisPackage/Main.java
+++ src/renameThisPackage/Main.java
@@ -4,14 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("ThiefGame");
-		//Planned use of Observer and Factory Method and probably decorator
-		//A thief "simulator" where you can use a gui to plan heists
-		//your goal is to generate wealth
-		//factory will create new heist opportunities
-		//observer will your character and will notify you about the happenings on the field
-		//decorator will have all kinds of things you can steal and have their values,
-		//have variants to the items, for example if there was a ring, it could be a
-		//gold ring, a silver ring and that would change it's value
 	}
 
 }
